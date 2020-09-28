d370bf4d24bd40512f03fefdab348042c289ea0c
idonia <sidoniag@gmail.com>
2020-09-27 20:59:30 -0500

    service worker successful

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,26 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/public/css/style.css',
+    '/public/js/index.js',
+    '/public/icons/icon-72x72.png',
+    '/public/icons/icon-96x96.png',
+    '/public/icons/icon-128x128.png',
+    '/public/icons/icon-144x144.png',
+    '/public/icons/icon-152x152.png',
+    '/public/icons/icon-192x192.png',
+    '/public/icons/icon-384x384.png',
+    '/public/icons/icon-512x512.png',
+];
 // install service worker
 self.addEventListener('install', function(evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
-            console.log('Your files were pre0caches successfully!');
+            console.log('Your files were pre-caches successfully!');
             return cache.addAll(FILES_TO_CACHE);
         })
     );
