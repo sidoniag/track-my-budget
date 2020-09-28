4587f99872e413d67e83d83342faf919291b4c65
idonia <sidoniag@gmail.com>
2020-09-28 10:03:01 -0500

    deploying

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI || 'mongodb://localhost/track-my-budget', {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
